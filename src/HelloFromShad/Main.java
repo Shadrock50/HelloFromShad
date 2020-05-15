db539d3bc6cbca193c5e69e10c82d5985e65613f
joewatt40 <wat19030@byui.edu>
2020-05-14 19:37:10 -0700

    Changes for Shad

--- src/HelloFromShad/Main.java
+++ src/HelloFromShad/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello from Shad!");
+        System.out.println("Hello Shad, this is Joe");
     }
 }
