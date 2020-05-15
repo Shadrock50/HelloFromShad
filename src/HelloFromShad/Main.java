6f74ed8bb4d7273b720a4f6feade38a8664e0223
Prydain <xelatath@hotmail.com>
2020-05-14 20:42:30 -0600

    Changes from Brian

--- src/HelloFromShad/Main.java
+++ src/HelloFromShad/Main.java
@@ -3,7 +3,8 @@ package HelloFromShad;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello from Shad!");
+        System.out.println("Hello Shad! ~Brian");
     }
 }
